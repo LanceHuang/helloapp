443765134d4f29ab7721d4c33b3b4632d5076211
Lance <634202275@qq.com>
2021-10-28 08:53:03 +0800

    First commit

---
+++ pages/index/index.vue
@@ -0,0 +1,29 @@
+<template>
+	<view class="content">
+		<!-- #ifdef H5 -->
+		<image src="../../static/img1.png" mode="widthFix"></image>
+		<!-- #endif -->
+		
+		<!-- #ifdef MP-WEIXIN -->
+		<image src="../../static/img2.png" mode="widthFix"></image>
+		<!-- #endif -->
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+	}
+</script>
+
+<style>
+	.content {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+	}
+</style>
